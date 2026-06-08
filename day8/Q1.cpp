84a6e727b51f432629f996aa6343edcccaa3a108
sakshamsinghbaish22 <sakshamsinghbaish22@gmail.com>
2026-06-08 12:36:13 +0300

    added Day 8 assignment

---
+++ day8/Q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for (i=1;i<=8;i++)
+    {
+        for (i=1;j<i;j++)
+        {
+            cout<<"*";
+        }
+   cout<<endl;
+    }
+    
+
+
+
+return 0;
+}
